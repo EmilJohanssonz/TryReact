3722f5f682cb26c5676ba4272557c38a983447fb
Emil Johansson <Emilaik@gmail.com>
2025-02-07 14:08:21 +0100

    h3 text font and italic

--- src/activity.tsx
+++ src/activity.tsx
@@ -5,6 +5,7 @@ function Activity() {
 
   return (
     <ul className="flex justify-center items-center flex-col p-6 -mt-42 space-y-4 bg-gradient-to-r from-blue-500 to-purple-500 rounded-lg shadow-xl max-w-xs mx-auto">
+    <h3 className="text-xl text-gray-50 font-mono italic">My favorite activity:</h3>
       <li className="text-lg font-semibold text-white bg-gray-800 px-4 py-2 rounded-full shadow-md">
         Football
       </li>
