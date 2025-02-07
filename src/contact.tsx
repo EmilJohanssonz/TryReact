2ed35eadbe5d605abf184fa0f6a20a0bc4b0861c
Emil Johansson <Emilaik@gmail.com>
2025-02-07 14:00:54 +0100

    fix label

--- src/contact.tsx
+++ src/contact.tsx
@@ -1,50 +1,37 @@
 function Contact() {
   return (
     <div className="max-w-md mx-auto bg-white p-8 rounded-lg shadow-lg mt-10">
-      <h2 className="text-2xl font-bold mb-6 text-center">Contact Us</h2>
+      <h2 className="text-2xl font-bold mb-6 text-center">Contact Me</h2>
       <form className="space-y-4">
         <div className="form-group">
-          <label
-            htmlFor="name"
-            className="block text-sm font-medium text-gray-700"
-          >
-            Name
-          </label>
-          <input
+                <input
+                placeholder="Name"
             type="text"
             id="name"
             name="name"
             required
-            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            className="placeholder-black  mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
           />
         </div>
         <div className="form-group">
-          <label
-            htmlFor="email"
-            className="block text-sm font-medium text-gray-700"
-          >
-            Email
-          </label>
+       
           <input
+          placeholder="Email"
             type="email"
             id="email"
             name="email"
             required
-            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            className=" placeholder-black mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
           />
         </div>
         <div className="form-group">
-          <label
-            htmlFor="message"
-            className="block text-sm font-medium text-gray-700"
-          >
-            Message
-          </label>
+       
           <textarea
+          placeholder="Message"
             id="message"
             name="message"
             required
-            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            className="placeholder-black mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
           ></textarea>
         </div>
         <button
